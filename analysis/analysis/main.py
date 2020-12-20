30afb12f30f5dc9481c50fd835b0505f231e5c57
Aaron <AaronBatilo@gmail.com>
2020-12-19 22:47:13 -0700

    Add 0.0 quantile

--- analysis/analysis/main.py
+++ analysis/analysis/main.py
@@ -2,13 +2,13 @@ import json
 import numpy
 
 bps = []
-with open('/home/aaron/Downloads/2020-12-20 (1).json', 'r') as input_file:
+with open('/home/aaron/Downloads/2020-12-20.json', 'r') as input_file:
     for interval in json.load(input_file)['intervals']:
         for stream in interval['streams']:
             bps += [int(stream['bits_per_second'])]
 
-quantiles = [0.1, 0.25, 0.5, 0.75, 0.9, 1]
+quantiles = [0.0, 0.1, 0.25, 0.5, 0.75, 0.9, 1.0]
 results = numpy.quantile(bps, quantiles)
 
 for i in range(len(quantiles)):
-    print(f'{quantiles[i]}\t--\t{results[i] / 1000000000:.3f} gigabits per second')
+    print(f'{quantiles[i]:.2f}\t--\t{results[i] / 1000000000:.3f} gigabits per second')
