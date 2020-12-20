9e133b377db764b2784db54a040e5a33a0649bd5
Aaron <AaronBatilo@gmail.com>
2020-12-20 10:38:36 -0700

    Parse more information for static props

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,14 +1,30 @@
 import { S3 } from "aws-sdk";
 
-type Props = {
+type PerInstanceProps = {
+  instanceType: string;
+  lastUpdated: string;
   baseline: number;
   burst: number;
 };
 
-const Home = ({ baseline, burst }: Props) => {
+type Props = {
+  instanceResults: PerInstanceProps[];
+};
+
+const Home = ({ instanceResults }: Props) => {
   return (
     <div>
-      Hello there! {baseline} -- {burst}
+      <ul>
+        {instanceResults.map((results) => {
+          const { instanceType, lastUpdated, baseline, burst } = results;
+          return (
+            <li>
+              {instanceType} has a baseline of {baseline} Gb/s and a burst of{" "}
+              {burst} Gb/s and was last updated on {lastUpdated}.
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
@@ -17,28 +33,88 @@ export async function getStaticProps() {
   const s3Client = new S3({ region: "us-east-1" });
 
   const res = await new Promise((resolve, reject) => {
-    s3Client.getObject(
+    s3Client.listObjectsV2(
       {
         Bucket: "ec2throughput.info",
-        Key: "results/c5-2xlarge/2020-12-20.json",
+        Delimiter: "/",
+        Prefix: "results/",
       },
-      (err, data) => {
+      async (err, data) => {
         if (err) reject(err);
-        const bps = JSON.parse(data.Body.toString("utf-8"))
-          .intervals.map((i: any) => i.sum)
-          .map((s: any) => s.bits_per_second / 1000000000);
-        resolve((bps as number[]).sort((a, b) => a - b));
+
+        const { CommonPrefixes: prefixes } = data;
+        const instanceTypes = prefixes.map((p) =>
+          p.Prefix.slice(8, p.Prefix.length - 1).replace("-", ".")
+        );
+
+        let allInstanceData = [];
+
+        for (let i = 0; i < prefixes.length; i++) {
+          const singleInstanceData = await new Promise((resolve, reject) => {
+            s3Client.listObjectsV2(
+              {
+                Bucket: "ec2throughput.info",
+                Prefix: prefixes[i].Prefix,
+              },
+              (err, data) => {
+                if (err) reject(err);
+                const { Key } = data.Contents[data.Contents.length - 1];
+
+                s3Client.getObject(
+                  {
+                    Bucket: "ec2throughput.info",
+                    Key,
+                  },
+                  (err, data) => {
+                    if (err) reject(err);
+                    const bps = JSON.parse(data.Body.toString("utf-8"))
+                      .intervals.map((i: any) => i.sum)
+                      .map((s: any) => s.bits_per_second / 1000000000);
+
+                    resolve({
+                      lastUpdated: Key.slice(
+                        Key.lastIndexOf("/") + 1,
+                        Key.lastIndexOf(".")
+                      ),
+                      instanceType: prefixes[i].Prefix.slice(
+                        8,
+                        prefixes[i].Prefix.length - 1
+                      ).replace("-", "."),
+                      baseline: bps[Math.floor(0.1 * (bps.length + 1))].toFixed(
+                        3
+                      ),
+                      burst: bps[Math.floor(0.97 * (bps.length + 1))].toFixed(
+                        3
+                      ),
+                    });
+                  }
+                );
+              }
+            );
+          });
+
+          console.log(singleInstanceData);
+          allInstanceData.push(singleInstanceData);
+        }
+        console.log("after for loop");
+        resolve(
+          allInstanceData.sort((a, b) => {
+            if (a.instanceType < b.instanceType) {
+              return -1;
+            }
+            if (a.instanceType > b.instanceType) {
+              return 1;
+            }
+            return 0;
+          })
+        );
       }
     );
   });
 
-  const bps: number[] = res as number[];
-  console.log(bps);
-
   return {
     props: {
-      baseline: bps[Math.floor(0.1 * (bps.length + 1))].toFixed(3),
-      burst: bps[Math.floor(0.97 * (bps.length + 1))].toFixed(3),
+      instanceResults: res,
     },
   };
 }
