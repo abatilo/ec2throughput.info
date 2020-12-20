f5d71766ed80d4965a5d1954885088eb771b4b15
Aaron <AaronBatilo@gmail.com>
2020-12-20 13:40:09 -0700

    Style website

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -13,18 +13,137 @@ type Props = {
 
 const Home = ({ instanceResults }: Props) => {
   return (
-    <div>
-      <ul>
-        {instanceResults.map((results) => {
-          const { instanceType, lastUpdated, baseline, burst } = results;
-          return (
-            <li key={instanceType}>
-              {instanceType} has a baseline of {baseline} Gb/s and a burst of{" "}
-              {burst} Gb/s and was last updated on {lastUpdated}.
-            </li>
-          );
-        })}
-      </ul>
+    <div className="min-h-screen px-4 py-2 bg-gray-200">
+      <h1 className="text-4xl font-extrabold text-yellow-600">
+        EC2Throughput.info
+      </h1>
+      <h2 className="text-2xl ">
+        Find how much network "Up to" actually means
+      </h2>
+      <main>
+        <section className="max-w-5xl p-2 mx-auto my-4 bg-yellow-500 rounded-lg">
+          <a href="https://twitter.com/dvassallo/status/1120171727399448576?s=20">
+            <blockquote className="italic">
+              TIL what EC2's "Up to" means. I used to think it simply indicates
+              best effort bandwidth, but apparently there's a hard baseline
+              bottleneck for most EC2 instance types (those with an "up to").
+              It's significantly smaller than the rating, and it can be reached
+              in just a few minutes.
+            </blockquote>
+            <figcaption>-- Daniel Vassallo (@dvassalo)</figcaption>
+          </a>
+        </section>
+        <div className="flex flex-col mx-8 mx-auto">
+          <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+              <div className="overflow-hidden border-b border-gray-200 shadow sm:rounded-lg">
+                <table className="min-w-full divide-y divide-gray-200">
+                  <thead className="bg-gray-50">
+                    <tr>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
+                      >
+                        Instance Type
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
+                      >
+                        Baseline (10% quantile)
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
+                      >
+                        Burst (97% quantile)
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
+                      >
+                        Last Updated
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {instanceResults.map((results, i) => {
+                      const {
+                        instanceType,
+                        lastUpdated,
+                        baseline,
+                        burst,
+                      } = results;
+                      return (
+                        <tr className={i % 2 != 0 ? "bg-gray-50" : "bg-white"}>
+                          <td className="px-6 py-4 text-sm font-medium text-gray-900 whitespace-nowrap">
+                            {instanceType}
+                          </td>
+                          <td className="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">
+                            {baseline} Gigabits / second
+                          </td>
+                          <td className="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">
+                            {burst} Gigabits / second
+                          </td>
+                          <td className="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">
+                            {lastUpdated}
+                          </td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer className="pt-6">
+        <p>
+          This website works by running an iperf3 test for an hour between two
+          instances in the same Availability Zone. This website takes{" "}
+          <span className="font-extrabold">heavy</span> inspiration from the{" "}
+          <a
+            href="https://twitter.com/dvassallo/status/1120171727399448576"
+            className="font-semibold text-yellow-600 hover:text-yellow-500"
+          >
+            @dvasallao Twitter thread
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://cloudonaut.io/ec2-network-performance-cheat-sheet/"
+            className="font-semibold text-yellow-600 hover:text-yellow-500"
+          >
+            this blog post from @andreaswittig / cloudonaut.io
+          </a>
+        </p>
+        <p>
+          <br />
+          This entire project and all of its benchmarking methods are open
+          source{" "}
+          <a
+            href="https://github.com/abatilo/ec2throughput.info"
+            className="font-semibold text-yellow-600 hover:text-yellow-500"
+          >
+            on GitHub.
+          </a>
+        </p>
+        <p>
+          You can ask for more instance types or make other suggestions by
+          opening an issue there.
+        </p>
+        <p>
+          <br />
+          If you'd like to help pay for other tests, I'd be grateful for a
+          donation of any amount{" "}
+          <a
+            href="https://www.paypal.me/abatilo"
+            className="font-semibold text-yellow-600 hover:text-yellow-500"
+          >
+            on PayPal.
+          </a>
+        </p>
+      </footer>
     </div>
   );
 };
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./src/**/*.html", "./src/**/*.js"],
+  purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
